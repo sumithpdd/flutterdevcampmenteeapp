dd0dc7f1262cf144923dac08f75e01647083561f
Sumith Damodaran <sumithpd@gmail.com>
2024-02-22 22:46:28 +0000

    Added firebase to the project

--- flutterdevcampmentee/lib/main.dart
+++ flutterdevcampmentee/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
 
@@ -13,9 +15,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: 1 == 1 ? LoginScreen() : HomeScreen(),
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: 1 == 1 ? LoginScreen() : HomeScreen(),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Container(),
+            ),
+          );
+        }
+      },
     );
   }
 }
